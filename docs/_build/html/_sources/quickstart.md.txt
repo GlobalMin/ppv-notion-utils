d1cb7f57813538e0d5a960318ed08c7ce73f1065
Jameqon Stillwell <jamesonstillwell@gmail.com>
2023-03-21 20:36:42 -0400

    docs

---
+++ docs/_build/html/_sources/quickstart.md.txt
@@ -0,0 +1,15 @@
+# Quickstart
+
+```{admonition} Copied from README.md
+:class: note
+:name: list-table-options
+
+1. Create a Notion API token to let Python interact with your Notion workspace. You can do this by going to [https://www.notion.so/my-integrations](https://www.notion.so/my-integrations) and creating a new integration. You'll need to give it a name and select the "Read and write" permission. Once you've created the integration, you'll be given a token that you can use to authenticate with Notion.
+2. Rename `.env.example` to `.env` and paste your token into the `NOTION_TOKEN` field.
+3. Fill in `WEEKS_DB` with the ID of the database that corresponds to the weekly review pages. This will allow you to bulk create new weeks in that db.
+4. Fill in `ACTION_ITEMS_DB` with the ID of the database that corresponds to the action items pages. This will allow you to bulk copy/paste recurring tasks at an 
+interval of your choosing.
+5. (Optoinal) Create a new virtual environment and install the dependencies with command `make install`. It's not a great idea to install dependencies globally on your machine but if you want you can simply run `pip install -r requirements.txt`.
+5. Open quick_start.ipynb for recurring tasks walkthrough.
+
+```
--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@ sys.path.insert(0, os.path.abspath(".."))
 from sphinx.application import Sphinx
 
 
-
 project = "PPV Notion utils"
 author = "Jameson Stillwell"
 root = "index"
@@ -25,10 +24,6 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx_design",
     "sphinx_copybutton",
-    # disabled due to https://github.com/mgaitan/sphinxcontrib-mermaid/issues/109
-    # "sphinxcontrib.mermaid",
-    "sphinxext.opengraph",
-    "sphinx_pyscript",
     "sphinx_tippy",
     "sphinx_togglebutton",
 ]
@@ -77,62 +72,61 @@ suppress_warnings = ["myst.strikethrough"]
 html_title = "PPV Notion Utils"
 
 
-html_show_copyright = False
 html_show_sphinx = False
+html_show_sourcelink = False
+html_show_copyright = False
+html_last_updated_fmt = None
+
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 html_css_files = ["local.css"]
-html_show_copyright = False
-html_theme = "sphinx_book_theme"
-html_last_updated_fmt = None
 html_logo = "_static/Untitled.png"
 
 html_theme_options = {
-    "use_download_button": False,
-    "use_fullscreen_button": False,
-    "home_page_in_toc": False,
-    "show_nav_level": 2,
-    "navigation_depth": 2
+    "external_links": [
+        {"name": "link-one-name", "url": "https://<link-one>"},
+        {"name": "link-two-name", "url": "https://<link-two>"},
+    ]
 }
-
 
 
 # -- Autodoc settings ---------------------------------------------------
 
 
-def setup(app: Sphinx):
-    """Add functions to the Sphinx setup."""
-    from myst_parser._docs import (
-        DirectiveDoc,
-        DocutilsCliHelpDirective,
-        MystAdmonitionDirective,
-        MystConfigDirective,
-        MystExampleDirective,
-        MystLexer,
-        MystToHTMLDirective,
-        MystWarningsDirective,
-        NumberSections,
-        StripUnsupportedLatex,
-    )
+# def setup(app: Sphinx):
+#     """Add functions to the Sphinx setup."""
+#     from myst_parser._docs import (
+#         DirectiveDoc,
+#         DocutilsCliHelpDirective,
+#         MystAdmonitionDirective,
+#         MystConfigDirective,
+#         MystExampleDirective,
+#         MystLexer,
+#         MystToHTMLDirective,
+#         MystWarningsDirective,
+#         NumberSections,
+#         StripUnsupportedLatex,
+#     )
 
-    app.add_directive("myst-config", MystConfigDirective)
-    app.add_directive("docutils-cli-help", DocutilsCliHelpDirective)
-    app.add_directive("doc-directive", DirectiveDoc)
-    app.add_directive("myst-warnings", MystWarningsDirective)
-    app.add_directive("myst-example", MystExampleDirective)
-    app.add_directive("myst-admonitions", MystAdmonitionDirective)
-    app.add_directive("myst-to-html", MystToHTMLDirective)
-    app.add_post_transform(StripUnsupportedLatex)
-    # app.add_post_transform(NumberSections)
-    app.connect("html-page-context", add_version_to_css)
-    app.add_lexer("myst", MystLexer)
+#     app.add_directive("myst-config", MystConfigDirective)
+#     app.add_directive("docutils-cli-help", DocutilsCliHelpDirective)
+#     app.add_directive("doc-directive", DirectiveDoc)
+#     app.add_directive("myst-warnings", MystWarningsDirective)
+#     app.add_directive("myst-example", MystExampleDirective)
+#     app.add_directive("myst-admonitions", MystAdmonitionDirective)
+#     app.add_directive("myst-to-html", MystToHTMLDirective)
+#     app.add_post_transform(StripUnsupportedLatex)
+#     # app.add_post_transform(NumberSections)
+#     app.connect("html-page-context", add_version_to_css)
+#     app.add_lexer("myst", MystLexer)
 
 
-def add_version_to_css(app, pagename, templatename, context, doctree):
-    """Add the version number to the local.css file, to bust the cache for changes."""
-    if app.builder.name != "html":
-        return
-    if "_static/local.css" in context.get("css_files", {}):
-        css = Path(app.srcdir, "_static/local.css").read_text("utf8")
-        hashed = hashlib.sha256(css.encode("utf-8")).hexdigest()
-        index = context["css_files"].index("_static/local.css")
-        context["css_files"][index] = f"_static/local.css?hash={hashed}"
+# def add_version_to_css(app, pagename, templatename, context, doctree):
+#     """Add the version number to the local.css file, to bust the cache for changes."""
+#     if app.builder.name != "html":
+#         return
+#     if "_static/local.css" in context.get("css_files", {}):
+#         css = Path(app.srcdir, "_static/local.css").read_text("utf8")
+#         hashed = hashlib.sha256(css.encode("utf-8")).hexdigest()
+#         index = context["css_files"].index("_static/local.css")
+#         context["css_files"][index] = f"_static/local.css?hash={hashed}"
